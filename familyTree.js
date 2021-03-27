42bf0a70e975034cfbeecee6e0cfed16d28c40b9
Glitch (proximal-organized-colossus) <none>
2021-03-27 04:11:09 +0000

    family album works now?

--- familyTree.js
+++ familyTree.js
@@ -3,5 +3,5 @@ var rootref = firebase.database().ref().child("Images");
 rootref.on("child_added", snap => {
   var image = snap.child("img").val();
   
-  $("#gallery_div").append("<img src=" + image + "></img>");
+  $("#gallery_div").append("<img src=" + image + ">");
 });
