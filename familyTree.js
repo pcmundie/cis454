217a368093c7dce554b42ec998cff9af8018d264
Glitch (proximal-organized-colossus) <none>
2021-03-30 12:15:31 +0000

    Cleaned code

--- familyTree.js
+++ familyTree.js
@@ -3,6 +3,6 @@ var rootref = firebase.database().ref("Images");
 rootref.on("child_added", snap => {
   var image = snap.child("img").val();
   console.log("image address: " + image);
-  
+
   $("#gallery_div").append("<img src=" + image + ">");
 });
