7eb67275ba1a839db5a892c9d9157a7e09e58fe3
Glitch (salty-buttercup-passenger) <none>
2021-04-01 18:34:01 +0000

    🚐🌂 Updated with Glitch

--- registerform.js
+++ registerform.js
@@ -1,41 +1,65 @@
-
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyA_-GXaiuJPnWOwrfIxuTtzrPJxh40Wi4c",
+    authDomain: "ancestree454.firebaseapp.com",
+    projectId: "ancestree454",
+    storageBucket: "ancestree454.appspot.com",
+    messagingSenderId: "996972698984",
+    appId: "1:996972698984:web:caafb3ddc770e314b14add",
+    measurementId: "G-YGH2EW22P2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+  
+  // Refernece contact infor collections
+let formmessage = firebase.database().ref('contactInfo');
 
 
 //listen for submit
-document.getElementById('contactInfo').addEventListener('submit' , submitForm);
+document.getElementById('.contactInfo').addEventListener('submit' , formSubmit);
 
-function submitForm(e) {
+sendToFirebase(fname, lname, email, pass, dobday, dobmonth, dobyear);
 
-  //get values
+function sendToFirebase(fname, lname, email, pass, dobday, dobmonth, dobyear) {
+  let newFormMessage = formMessage.push();
+  newFormMessage.set({
+      First: fname,
+      Last: lname,
+      Email: email,
+      Password: pass,
+      Day: dobday,
+      Month: dobmonth,
+      Year: dobyear,
 
-  var fname= getInputVal('fname');
-  var lname= getInputVal('lname');
-  var email= getInputVal('email');
-  var dobday= getInputVal('dobday');
-  var dobmonth= getInputVal('dobmonth');
-  var dobyear= getInputVal('dobyear');
-  var pass= getInputVal('pass');
-  saveMessage(fname, lname, email, dobday, dobmonth, dobyear, pass);
-  document.getElementById('contactInfo').reset();
+  });
 
+  function formSubmit(e) {
+    e.preventDefault();
+    //get values
+    let fname = document.querySelector(".fname").value;
+    let lname = document.querySelector(".lname").value;
+    let email = document.querySelector(".email").value;
+    let pass = document.querySelector(".pass").value;
+    let dobday = document.querySelector(".dobday").value;
+    let dobmonth = document.querySelector(".dobmonth").value;
+    let dobyear = document.querySelector(".dobyear").value;
 
+    document.getElementById('newaccount').reset();
+
+    
+  }
+//Save infos to firebase
+var query = firebase.database().ref("contactInfo").orderByKey();
+    query.once("value")
+        .then(function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                
+                var key = childSnapshot.key;
+                var childData = childSnapshot.val();
+                //trying to post it all in one container, but not sure if this is correct yet. Testing needed
+                document.getElementById("postContainer").appendChild(childData.First + "\n" + childData.Last + "\n" + childData.Email + "\n"+ childData.Password + "\n"+ childData.Day + "\n"+ childData.Month + "\n"+ childData.Year + "\n");
+                
+            });
+        });
 }
-//get form values
-function getInputVal(id){
-  return document.getElementById(id).value;
-}
-
-//save info in database
-function saveMessage(fname, lname, email, dobday, dobmonth, dobyear, pass){
-  var formMessage = firebase.databse().ref('Register');
-  formMessage.push({
-    Firstname: fname,
-    Lastname: lname,
-    Email: email,
-    Day: dobday,
-    Month: dobmonth,
-    Year: dobyear,
-    Password: pass,
-  })
-}
-
--- updates.js
+++ updates.js
@@ -1,27 +1,6 @@
 
-//Get data after submit button is clicked
 document.getElementById('newPost').addEventListener('submit', submitForm);
-//initalize empty arrays to store info from reading database
-var captionArray = [];
-var textArray = [];
-//add image functionality if time
-var imgArray = [];
-var placeInArray = 0;
 
-//Display and cycle through posts when next is clicked
-next.addEventListener("click", function () {
-    //returns to newest post if oldest post is reached
-    if (captionArray.length <= placeInArray) {
-        placeInArray = 0;
-    }
-    document.getElementById("next").innerHTML = "Next Post";
-    document.getElementById("like").innerHTML = "Like";
-    document.getElementById("displayCaption").innerHTML = captionArray[placeInArray];
-    document.getElementById("displayText").innerHTML = textArray[placeInArray];
-    placeInArray = placeInArray + 1;
-});
-//Click next manually so initial post is displayed
-//When submit is clicked
 function submitForm(e) {
     e.preventDefault();
 
@@ -29,9 +8,7 @@ function submitForm(e) {
     var name = getInputVal('caption');
     var email = getInputVal('post');
     var photo = getInputVal('postPhoto');
-    //gets inputs and sends to database
     saveMessage(name, email, photo);
-    //clears form
     document.getElementById('newPost').reset();
 }
 
@@ -42,6 +19,7 @@ function getInputVal(id) {
 
 // Save message to firebase
 function saveMessage(title, text, photo) {
+    //var newMessageRef = formMessage.push();
     var formMessage = firebase.database().ref('Updates');
     formMessage.push({
         Title: title,
@@ -49,8 +27,7 @@ function saveMessage(title, text, photo) {
         Image: photo,
     });
 }
-
-//Gets already uploaded posts from database
+/*
 var query = firebase.database().ref("Updates").orderByKey();
 query.once("value")
     .then(function (snapshot) {
@@ -59,11 +36,7 @@ query.once("value")
             var key = childSnapshot.key;
             var childData = childSnapshot.val();
             //trying to post it all in one container, but not sure if this is correct yet. Testing needed
-            captionArray.push(childData.Title);
-            textArray.push(childData.Text);
-            imgArray.push(childData.Image);
-            
-           
+            document.getElementById("postContainer").appendChild(childData.Title + "\n" + childData.text + "\n" + childData.Image + "\n");
 
         });
-    }); 
+    }); */
