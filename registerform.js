9cd29b13fce7f32d16395ee29af6f3deaf8bd1b1
Fiona PB <59042926+fcpowe@users.noreply.github.com>
2021-04-01 19:53:03 -0400

    HTML Errors updated and null error fixed

--- registerform.js
+++ registerform.js
@@ -1,21 +1,40 @@
 
-
+ document.getElementById('contactInfo').addEventListener('submit', submitForm);
 
 //listen for submit
-document.getElementById('contactInfo').addEventListener('submit' , submitForm);
 
 function submitForm(e) {
+    console.log("test 2");
+    var x = document.forms["contactInfo"]["fname"].value;
+    var y = document.forms["contactInfo"]["lname"].value;
+    var e = document.forms["contactInfo"]["email"].value;
+    var p1 = document.forms["contactInfo"]["pass"].value;
+    var p2 = document.forms["contactInfo"]["pass2"].value;
+    if (x == "" || y == "") {
+        alert("Name must be filled out, correctly");
+        return false;
+    }
+    if (e == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    if (p1 != p2) {
+        alert("Passwords do not match");
+        return false;
+    }
 
   //get values
-
+    console.log("test 3");
   var fname= getInputVal('fname');
   var lname= getInputVal('lname');
-  var email= getInputVal('email');
+    var email = getInputVal('email');
+    console.log("test 4");
   var dobday= getInputVal('dobday');
   var dobmonth= getInputVal('dobmonth');
   var dobyear= getInputVal('dobyear');
-  var pass= getInputVal('pass');
-  saveMessage(fname, lname, email, dobday, dobmonth, dobyear, pass);
+  var pass= getInputVal('pass'); 
+    saveMessage(fname, lname, email, dobday, dobmonth, dobyear, pass);
+    console.log("test 5");
   document.getElementById('contactInfo').reset();
 
 
@@ -27,15 +46,15 @@ function getInputVal(id){
 
 //save info in database
 function saveMessage(fname, lname, email, dobday, dobmonth, dobyear, pass){
-  var formMessage = firebase.databse().ref('Register');
-  formMessage.push({
-    Firstname: fname,
-    Lastname: lname,
-    Email: email,
-    Day: dobday,
-    Month: dobmonth,
-    Year: dobyear,
-    Password: pass,
-  })
+  var formMessage = firebase.database().ref('Register');
+    formMessage.push({
+        Firstname: fname,
+        Lastname: lname,
+        Email: email,
+        Day: dobday,
+        Month: dobmonth,
+        Year: dobyear,
+        Password: pass,
+    });
 }
 
