82f302d5ea7970fcd855670e13404b8e517750a8
Glitch (proximal-organized-colossus) <none>
2021-04-03 21:02:47 +0000

    credited sources

--- familyTree.js
+++ familyTree.js
@@ -1,3 +1,4 @@
+//Image pull from the database code adapted from: https://himanshufi.medium.com/get-images-from-your-firebase-database-to-your-gallery-in-a-website-9f02efb41d33
 var rootref = firebase.database().ref('Images');
 
 rootref.on("child_added", snap => {
