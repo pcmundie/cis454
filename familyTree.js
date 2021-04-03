c478afcb763d9eb0af85ac08e58755a4192d5076
Glitch (proximal-organized-colossus) <none>
2021-04-03 20:56:32 +0000

    Fixed Family Album Finally!!!!

--- familyTree.js
+++ familyTree.js
@@ -1,8 +1,7 @@
-var rootref = firebase.database().ref("Images");
+var rootref = firebase.database().ref('Images');
 
 rootref.on("child_added", snap => {
   var image = snap.child("img").val();
-  console.log("image address: " + image);
 
   $("#gallery_div").append("<img src=" + image + ">");
 });
